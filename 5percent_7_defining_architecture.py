aebd539b4efca6676db826f8026a70e751ffea66
David Sterry <davesterry@gmail.com>
2022-09-29 10:14:23 -0700

    finished but took at least an hour on cpu

---
+++ 5percent_7_defining_architecture.py
@@ -0,0 +1,98 @@
+import cv2    # for capturing videos
+import math   # for mathematical operations
+import matplotlib.pyplot as plt    # for plotting the images
+# %matplotlib inline   # possibly a config thing that I can do another way -DRS
+import pandas as pd
+from keras.preprocessing import image   # for preprocessing the images
+import numpy as np    # for mathematical operations
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.applications.vgg16 import VGG16
+from keras.layers import Dense, InputLayer, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D
+from skimage.transform import resize   # for resizing images
+from sklearn.model_selection import train_test_split
+from glob import glob
+from tqdm import tqdm
+
+# open the .txt file which have names of training videos
+f = open("ucfTrainTestlist/5percent_trainlist01.txt", "r")
+temp = f.read()
+videos = temp.split('\n')
+
+# creating a dataframe having video names
+train = pd.DataFrame()
+train['video_name'] = videos
+train = train[:-1]
+#print(train.head())
+
+# creating tags for training videos
+train_video_tag = []
+for i in range(train.shape[0]):
+    train_video_tag.append(train['video_name'][i].split('/')[0])
+    
+train['tag'] = train_video_tag
+#print(train.head())
+
+
+
+# getting the names of all the images
+images = glob("5percent_train_1/*.jpg")
+train_image = []
+train_class = []
+for i in tqdm(range(len(images))):
+    # creating the image name
+    train_image.append(images[i].split('/')[1])
+    # creating the class of image
+    train_class.append(images[i].split('/')[1].split('_')[1])
+    
+# storing the images and their class in a dataframe
+train_data = pd.DataFrame()
+train_data['image'] = train_image
+train_data['class'] = train_class
+
+# converting the dataframe into csv file 
+train_data.to_csv('Videos/5percent_train_new.csv',header=True, index=False)
+
+print(train_data.shape[0])
+
+# creating an empty list
+train_image = []
+
+# for loop to read and store frames
+for i in tqdm(range(int(train_data.shape[0]))):
+    # loading the image and keeping the target size as (224,224,3)
+    img = image.load_img('train_1/'+train_data['image'][i], target_size=(224,224,3))
+    # converting it to array
+    img = image.img_to_array(img)
+    # normalizing the pixel value
+    img = img/255
+    # appending the image to the train_image list
+    train_image.append(img)
+    
+# converting the list to numpy array
+X = np.array(train_image)
+del train_image
+
+# separating the target
+y = train_data['class']
+
+# creating the training and validation set
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2, stratify = y)
+
+# creating dummies of target variable for train and validation set
+y_train = pd.get_dummies(y_train)
+y_test = pd.get_dummies(y_test)
+
+
+
+# creating the base model of pre-trained VGG16 model
+base_model = VGG16(weights='imagenet', include_top=False)
+
+# extracting features for training frames
+X_train = base_model.predict(X_train)
+print(X_train.shape)
+
+# extracting features for validation frames
+X_test = base_model.predict(X_test)
+print(X_test.shape)
